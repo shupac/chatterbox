330f5f560d48674163937fb76c33666e39513774
Hack Reactor Students <github@hackreactor.com>
2013-10-07 21:48:14 -0700

    Implemented chat rooms and user can send msgs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,15 @@
 // YOUR CODE HERE:
 var useURL = 'https://api.parse.com/1/classes/chatterbox';
 
-var msgResults;
+var _msgResults;
 var lastMsgTime = 0;
+var filter;
 
 var sendChat = function() {
   var msg = {
     "username": 'Shu',
     "text": $('.msgInput').val(),
-    'roomname': '4chan'
+    'roomname': filter
   };
   var stringified = JSON.stringify(msg);
   $.ajax({
@@ -27,28 +28,50 @@ var retrieve = function(room) {
   $.ajax({
     url : useURL,
     type : 'GET',
-    // data : 'order=-createdAt limit=10',
     data : {
       order: "-createdAt",
-      limit: 10,
-      keys: 'roomname'
+      limit: 200
     },
     success : function(data) {
-      msgResults = data.results;
-      display(data.results);
+      _msgResults = data.results;
+      displayByRoom(filter);
+      buildChatRooms();
     }
   });
 };
 
-var display = function(data) {
-  $('ul').text('');
-  _(data).each(function(msgData) {
-    $('.chatMsgs').prepend('<li>' + escapeString(msgData.username, msgData) + ': ' + escapeString(msgData.text, msgData) + '</li>');
+var buildChatRooms = function() {
+  var chatRooms = {};
+  $('.rooms').text('');
+  for (var i = 0; i < _msgResults.length; i++) {
+    var room = _msgResults[i].roomname;
+    if (!chatRooms[room]) {
+      chatRooms[room] = 1;
+      $('.rooms').append('<p><a href="#" class="roomLink">' + escapeString(room) + '</a></p>');
+    } else {
+      chatRooms[room]++;
+    }
+  }
+  $('a.roomLink').on('click', function(){
+    filter = $(this).text();
+    displayByRoom( filter );
   });
- };
+  console.log(chatRooms);
+  return chatRooms;
+};
+
+var displayByRoom = function(target) {
+  target = target || 'lobby';
+  $('ul.chatMsgs').text('');
+  var filtered = _(_msgResults).each(function(msgData){
+    if (msgData.roomname === target) {
+      $('.chatMsgs').prepend('<li>' + escapeString(msgData.username, msgData) + ': ' + escapeString(msgData.text, msgData) + '</li>');
+    }
+  });
+};
 
 var escapeString = function(string, data) {
-  if (string !== undefined) {
+  if (string !== undefined && string !== null) {
     var returnString = '';
     var specChars = {
      "&": "&amp",
@@ -79,6 +102,7 @@ $(document).ready(function() {
       sendChat();
     }
   });
+
 });
 
 // $.ajax({
